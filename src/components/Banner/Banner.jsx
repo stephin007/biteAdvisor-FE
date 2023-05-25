bb4cf63e3ff6d373730f5b6c9abb6f6db58f16b1
Stephin <stephinreji123@gmail.com>
2023-05-25 21:49:26 +0530

    fixed Initial CSS setup

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,13 +3,13 @@ import "./banner.css";
 const Banner = () => {
   return (
     <>
-      <section className='banner'>
+      <div className='banner'>
         <h2>Taste. Rate. Savor. Share.</h2>
         <p>
           Indulge Your Taste Buds and Discover Culinary Delights! Welcome to
           SavorySights - Your Ultimate Destination for Restaurant Reviews!
         </p>
-      </section>
+      </div>
     </>
   );
 };
