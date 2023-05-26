b4f7a31d1ec348cd91ad1358913632e593ca23e5
Stephin <stephinreji123@gmail.com>
2023-05-26 09:19:42 +0530

    added .env file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import { useState, useEffect } from "react";
+import db from "./firebase";
+import { collection, getDocs } from "firebase/firestore";
+
 import "./App.css";
 import { Header, Banner, Search } from "./components";
 
 const App = () => {
+  const [reviews, setReviews] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const restaurantDetailsRef = collection(db, "details");
+
+  useEffect(() => {
+    const getDetails = async () => {
+      setLoading(true);
+      const data = await getDocs(restaurantDetailsRef);
+      setReviews(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      console.log(reviews);
+      console.log(loading);
+      setLoading(false);
+    };
+
+    getDetails();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <div className='container'>
       <div className='header-container'>
